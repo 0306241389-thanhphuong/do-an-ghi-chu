3ce8e15c17e15c9f3b158925f486bff37876b5d1
thanhphuong2k6 <thanhphuong1152006@gmail.com>
2026-09-19 15:32:01 +0700

    feat: Hoan thanh Sprint 1 - Tinh nang Profile va Setup du an

---
+++ frontend/src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+// import App from './App.jsx'
+import Settings from "./Settings";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Settings />
+  </StrictMode>,
+);
