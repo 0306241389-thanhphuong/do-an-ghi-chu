50c40064b4f7769ebe01f332c5c8a727e9110882
0306241380 <0306241380@caothang.edu.vn>
2026-09-21 18:23:25 +0700

    Giao diên ghi chú(Sprint 2)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 // import App from './App.jsx'
-import Settings from "./Settings";
+// import Settings from "./Settings";
+import Notes from "./Notes";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Settings />
+    {/* <Settings  */}
+    <Notes />
   </StrictMode>,
 );
